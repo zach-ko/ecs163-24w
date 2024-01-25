95fb841ae5782be799885fa38f3094dfd7b0fcfc
Jessica Chen <71425321+yingchengchen@users.noreply.github.com>
2024-01-26 04:38:47 +0800

    Add template for hw2

---
+++ Homework2/main.js
@@ -0,0 +1,208 @@
+let abFilter = 25
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+let scatterLeft = 0, scatterTop = 0;
+let scatterMargin = {top: 10, right: 30, bottom: 30, left: 60},
+    scatterWidth = 400 - scatterMargin.left - scatterMargin.right,
+    scatterHeight = 350 - scatterMargin.top - scatterMargin.bottom;
+
+let distrLeft = 400, distrTop = 0;
+let distrMargin = {top: 10, right: 30, bottom: 30, left: 60},
+    distrWidth = 400 - distrMargin.left - distrMargin.right,
+    distrHeight = 350 - distrMargin.top - distrMargin.bottom;
+
+let teamLeft = 0, teamTop = 400;
+let teamMargin = {top: 10, right: 30, bottom: 30, left: 60},
+    teamWidth = width - teamMargin.left - teamMargin.right,
+    teamHeight = height-450 - teamMargin.top - teamMargin.bottom;
+
+
+d3.csv("players.csv").then(rawData =>{
+    console.log("rawData", rawData);
+    
+    rawData.forEach(function(d){
+        d.AB = Number(d.AB);
+        d.H = Number(d.H);
+        d.salary = Number(d.salary);
+        d.SO = Number(d.SO);
+    });
+    
+
+    rawData = rawData.filter(d=>d.AB>abFilter);
+    rawData = rawData.map(d=>{
+                          return {
+                              "H_AB":d.H/d.AB,
+                              "SO_AB":d.SO/d.AB,
+                              "teamID":d.teamID,
+                          };
+    });
+    console.log(rawData);
+    
+//plot 1
+    const svg = d3.select("svg")
+
+    const g1 = svg.append("g")
+                .attr("width", scatterWidth + scatterMargin.left + scatterMargin.right)
+                .attr("height", scatterHeight + scatterMargin.top + scatterMargin.bottom)
+                .attr("transform", `translate(${scatterMargin.left}, ${scatterMargin.top})`)
+
+    // X label
+    g1.append("text")
+    .attr("x", scatterWidth / 2)
+    .attr("y", scatterHeight + 50)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .text("H/AB")
+    
+
+    // Y label
+    g1.append("text")
+    .attr("x", -(scatterHeight / 2))
+    .attr("y", -40)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .text("SO/AB")
+
+    // X ticks
+    const x1 = d3.scaleLinear()
+    .domain([0, d3.max(rawData, d => d.H_AB)])
+    .range([0, scatterWidth])
+
+    const xAxisCall = d3.axisBottom(x1)
+                        .ticks(7)
+    g1.append("g")
+    .attr("transform", `translate(0, ${scatterHeight})`)
+    .call(xAxisCall)
+    .selectAll("text")
+        .attr("y", "10")
+        .attr("x", "-5")
+        .attr("text-anchor", "end")
+        .attr("transform", "rotate(-40)")
+
+    // Y ticks
+    const y1 = d3.scaleLinear()
+    .domain([0, d3.max(rawData, d => d.SO_AB)])
+    .range([scatterHeight, 0])
+
+    const yAxisCall = d3.axisLeft(y1)
+                        .ticks(13)
+    g1.append("g").call(yAxisCall)
+
+    const rects = g1.selectAll("circle").data(rawData)
+
+    rects.enter().append("circle")
+         .attr("cx", function(d){
+             return x1(d.H_AB);
+         })
+         .attr("cy", function(d){
+             return y1(d.SO_AB);
+         })
+         .attr("r", 3)
+         .attr("fill", "#69b3a2")
+
+//space
+    const g2 = svg.append("g")
+                .attr("width", distrWidth + distrMargin.left + distrMargin.right)
+                .attr("height", distrHeight + distrMargin.top + distrMargin.bottom)
+                .attr("transform", `translate(${distrLeft}, ${distrTop})`)
+
+//plot 2
+    
+    q = rawData.reduce((s, { teamID }) => (s[teamID] = (s[teamID] || 0) + 1, s), {});
+    r = Object.keys(q).map((key) => ({ teamID: key, count: q[key] }));
+    console.log(r);
+
+           
+    const g3 = svg.append("g")
+                .attr("width", teamWidth + teamMargin.left + teamMargin.right)
+                .attr("height", teamHeight + teamMargin.top + teamMargin.bottom)
+                .attr("transform", `translate(${teamMargin.left}, ${teamTop})`)
+
+    // X label
+    g3.append("text")
+    .attr("x", teamWidth / 2)
+    .attr("y", teamHeight + 50)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .text("Team")
+    
+
+    // Y label
+    g3.append("text")
+    .attr("x", -(teamHeight / 2))
+    .attr("y", -40)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .text("Number of players")
+
+    // X ticks
+    const x2 = d3.scaleBand()
+    .domain(r.map(d => d.teamID))
+    .range([0, teamWidth])
+    .paddingInner(0.3)
+    .paddingOuter(0.2)
+
+    const xAxisCall2 = d3.axisBottom(x2)
+    g3.append("g")
+    .attr("transform", `translate(0, ${teamHeight})`)
+    .call(xAxisCall2)
+    .selectAll("text")
+        .attr("y", "10")
+        .attr("x", "-5")
+        .attr("text-anchor", "end")
+        .attr("transform", "rotate(-40)")
+
+    // Y ticks
+    const y2 = d3.scaleLinear()
+    .domain([0, d3.max(r, d => d.count)])
+    .range([teamHeight, 0])
+
+    const yAxisCall2 = d3.axisLeft(y2)
+                        .ticks(6)
+    g3.append("g").call(yAxisCall2)
+
+    const rects2 = g3.selectAll("rect").data(r)
+
+    rects2.enter().append("rect")
+    .attr("y", d => y2(d.count))
+    .attr("x", (d) => x2(d.teamID))
+    .attr("width", x2.bandwidth)
+    .attr("height", d => teamHeight - y2(d.count))
+    .attr("fill", "grey")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}).catch(function(error){
+    console.log(error);
+});
+
